3220fbcb02aebca36842fee2326e4429b15ddaa6
scnb <18633910528@163.com>
2023-05-09 23:50:38 +0800

    开发4个基本的路由组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入路由
+import router from '@/router'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //注册路由：底下的写法是kv一致省略v，注意router小写的
+  router
 }).$mount('#app')

8d93a4717698251e3ebb9533c521c155fb1ff0ed
scnb <18633910528@163.com>
2023-05-22 23:41:23 +0800

    8.路由传递参数

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -33,8 +33,8 @@
                 </router-link>
             </h1>
             <div class="searchArea">
-                <form action="###" class="searchForm">
-                    <input type="text" id="autocomplete" class="input-error input-xxlarge" />
+                <form action="###" class="searchForm" :keyword="keyword">
+                    <input type="text" id="autocomplete" class="input-error input-xxlarge" v-model="keyword" />
                     <button class="sui-btn btn-xlarge btn-danger" type="button" @click="goSearch">搜索</button>
                 </form>
             </div>
@@ -45,10 +45,21 @@
 <script>
 export default {
     name: "",
+    data() {
+        return {
+            keyword: ''
+        }
+    },
     methods: {
         // 搜索按钮的回调函数：需要向search路由进行跳转
         goSearch() {
-            this.$router.push("/search");
+            // 路由传递参数
+            // 第一种：字符串形式
+            // this.$router.push("/search/" + this.keyword + "?k=" + this.keyword.toUpperCase());
+            // 第二种：模板字符串
+            // this.$router.push(`/search/${this.keyword}?k=${this.keyword.toUpperCase()}`)
+            // 第三种：对象
+            this.$router.push({name: 'search', params: {keyword: this.keyword}, query: {k: this.keyword.toUpperCase()}})
         },
     }
 }
